2f0f9472c300d89b179f5d8b3bd44304d595c965
Arianna Cipolla <arianna.cipolla@studenti.unipr.it>
2026-03-18 11:04:35 +0100

    feat: installazione e configurazione Vercel Speed Insights

--- my-portfolio/app/layout.tsx
+++ my-portfolio/app/layout.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -59,6 +60,7 @@ export default function RootLayout({
     <html lang="it" className="scroll-smooth overscroll-none">
       <body className={`${saira.variable} antialiased`}>
         {children}
+        <SpeedInsights />
       </body>
     </html>
   );
